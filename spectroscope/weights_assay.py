73e3f152736064fb10989d9a0007c3eb17bff45c
Rumi Salazar <76025024+rumi-salazar@users.noreply.github.com>
2023-04-16 17:08:10 +1000

    ADD singular values probe

--- spectroscope/weights_assay.py
+++ spectroscope/weights_assay.py
@@ -1,5 +1,8 @@
-from typing import Iterable
-
+from typing import Iterable, Type
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch as t
 
 NUM_STEPS = 792
 IVL = 24
@@ -20,11 +23,16 @@ class WeightsAssay:
     
     def _measure(self, model, step):
         weight_norms = self.weight_norms(model)
+        singular_values = self.singular_values(model)
+        max_singular_values = [max(sv_layer) for sv_layer in singular_values]
 
         return {
             "step": step,
             "weight_norms": weight_norms,
             "total_weight_norm": sum([wn ** 2 for wn in weight_norms]) ** 0.5,
+            "singular_values": singular_values,
+            "max_singular_values": max_singular_values, #maximum singular value in each layer
+            "prod_max_singular_values": np.prod(max_singular_values),
             # "last_layer_rank": self.last_layer_rank(model),
         }
     
@@ -36,6 +44,21 @@ class WeightsAssay:
             norms.append(p.norm().item())
 
         return norms
+
+    # This method finds the singular values for each layer in the network
+    @classmethod
+    def singular_values(cls, model: nn.Module):
+        singular_values = []
+        for name, param in model.named_parameters():
+            if "weight" in name:
+                if "conv" in name:
+                    reshaped_param = param.data.view(param.size(0), -1)
+                elif "fc" in name:
+                    reshaped_param = param.data
+                _, s, _ = t.svd(reshaped_param)
+                singular_values.append(s.tolist())
+        
+        return singular_values
         
     # @classmethod
     # def last_layer_rank(cls, model: nn.Module):
